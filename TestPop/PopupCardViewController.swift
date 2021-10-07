fd4a668e35b54dd665bb69c9f93e57af782ee46e
abrahamshenghur <ashenghur@gmail.com>
2021-10-06 17:32:58 -0700

    Create popup card VC  and change background color

---
+++ TestPop/PopupCardViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PopupCardViewController.swift
+//  TestPop
+//
+//  Created by John on 10/6/21.
+//  Copyright © 2021 Abraham Shenghur. All rights reserved.
+//
+
+import UIKit
+
+class PopupCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
