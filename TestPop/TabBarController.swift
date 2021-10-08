e56a3ab2c76e9c1864ba8ccc53cf7769574f1a8d
abrahamshenghur <ashenghur@gmail.com>
2021-10-08 16:07:30 -0700

    iMac - initialize tab bar controller

---
+++ TestPop/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  TestPop
+//
+//  Created by Abraham on 10/8/21.
+//  Copyright © 2021 Abraham Shenghur. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // iMac - Initialize a tab bar controller
+    }
+}
