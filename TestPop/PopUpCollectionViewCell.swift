d7a886db46864e8d5a736d6f61fd25992db4c50f
abrahamshenghur <ashenghur@gmail.com>
2021-10-08 11:48:42 -0700

    create cell for collection view

---
+++ TestPop/PopUpCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PopUpCollectionViewCell.swift
+//  TestPop
+//
+//  Created by John on 10/8/21.
+//  Copyright © 2021 Abraham Shenghur. All rights reserved.
+//
+
+import UIKit
+
+class PopUpCollectionViewCell: UICollectionViewCell {
+    // Create this collection view cell
+}
