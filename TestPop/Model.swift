d3e5a215c98751f76c5d2eff23d967b8194089f2
abrahamshenghur <ashenghur@gmail.com>
2021-10-08 15:40:01 -0700

    iMac - Create model file

---
+++ TestPop/Model.swift
@@ -0,0 +1,11 @@
+//
+//  Model.swift
+//  TestPop
+//
+//  Created by Abraham on 10/8/21.
+//  Copyright © 2021 Abraham Shenghur. All rights reserved.
+//
+
+import Foundation
+
+// Created this model on the iMac
